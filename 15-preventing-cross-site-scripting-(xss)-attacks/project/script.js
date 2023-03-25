717f5ae5adbb38c90b1276ccbb8c197748623339
r4deadv <mihovilovic.rade@gmail.com>
2023-03-26 00:22:06 +0100

    project 15 competed

--- 15-preventing-cross-site-scripting-(xss)-attacks/project/script.js
+++ 15-preventing-cross-site-scripting-(xss)-attacks/project/script.js
@@ -1,3 +1,87 @@
+/**
+ * Sanitize an HTML string
+ * @param  {String}          str   The HTML string to sanitize
+ * @param  {Boolean}         nodes If true, returns HTML nodes instead of a string
+ * @return {String|NodeList}       The sanitized string or nodes
+ */
+function cleanHTML(str, nodes) {
+  /**
+   * Convert the string to an HTML document
+   * @return {Node} An HTML document
+   */
+  function stringToHTML() {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(str, "text/html");
+    return doc.body || document.createElement("body");
+  }
+
+  /**
+   * Check if the attribute is potentially dangerous
+   * @param  {String}  name  The attribute name
+   * @param  {String}  value The attribute value
+   * @return {Boolean}       If true, the attribute is potentially dangerous
+   */
+  function isPossiblyDangerous(name, value) {
+    let val = value.replace(/\s+/g, "").toLowerCase();
+    if (["src", "href", "xlink:href"].includes(name)) {
+      if (val.includes("javascript:") || val.includes("data:text/html"))
+        return true;
+    }
+    if (name.startsWith("on")) return true;
+  }
+
+  /**
+   * Remove potentially dangerous attributes from an element
+   * @param  {Node} elem The element
+   */
+  function removeAttributes(elem) {
+    // If the node is not an element, bail
+    if (elem.nodeType !== 1) return;
+
+    // Otherwise, loop through each attribute
+    // If it's dangerous, remove it
+    let atts = elem.attributes;
+    for (let { name, value } of atts) {
+      if (!isPossiblyDangerous(name, value)) continue;
+      elem.removeAttribute(name);
+    }
+  }
+
+  /**
+   * Remove <script> elements
+   * @param  {Node} html The HTML
+   */
+  function removeScripts(html) {
+    let scripts = html.querySelectorAll("script");
+    for (let script of scripts) {
+      script.remove();
+    }
+  }
+
+  /**
+   * Remove dangerous stuff from the HTML document's nodes
+   * @param  {Node} html The HTML document
+   */
+  function cleanHTML(html) {
+    let nodes = html.childNodes;
+    for (let node of nodes) {
+      removeAttributes(node);
+      cleanHTML(node);
+    }
+  }
+
+  // Convert the string to HTML
+  let html = stringToHTML();
+
+  // Sanitize it
+  cleanHTML(html);
+  removeScripts(html);
+
+  // If the user wants HTML nodes back, return them
+  // Otherwise, pass a sanitized string back
+  return nodes ? html.childNodes : html.innerHTML;
+}
+
 // Get the DOM nodes
 let github = document.querySelector("#github");
 
@@ -21,7 +105,7 @@ function render(data) {
   repos = repos.slice(0, 5);
 
   // Render the UI
-  github.innerHTML = `<div class="gh-grid">
+  github.innerHTML = cleanHTML(`<div class="gh-grid">
         <div class="gh-avatar">
           <p><img alt="" src="${profile.avatar_url}"></p>
         </div>
@@ -41,7 +125,7 @@ function render(data) {
               .join("")}
           </ul>
         </div>
-      </div>`;
+      </div>`);
 }
 
 // Call the GitHub APIs
